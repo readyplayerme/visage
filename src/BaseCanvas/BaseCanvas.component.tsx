ada1f64eda0091cc8110683793c70ba47000a8ef
Elar Huik <ehexec@gmail.com>
2022-04-22 13:02:52 +0300

    feat: add ability to adjust camera position

--- src/BaseCanvas/BaseCanvas.component.tsx
+++ src/BaseCanvas/BaseCanvas.component.tsx
@@ -1,19 +1,26 @@
 import React, { ReactNode, FC } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Vector3 } from 'three';
 import styles from './BaseCanvas.module.css';
 
 interface BaseCanvasProps {
   children?: ReactNode;
   background?: string;
   fov?: number;
+  cameraPosition?: Vector3;
 }
 
-export const BaseCanvas: FC<BaseCanvasProps> = ({ children = undefined, background = '#0e0f1e', fov = 50 }) => (
+export const BaseCanvas: FC<BaseCanvasProps> = ({
+  children = undefined,
+  background = '#0e0f1e',
+  fov = 50,
+  cameraPosition = new Vector3(0, 0, 5)
+}) => (
   <Canvas
     className={styles['base-canvas']}
     shadows
     dpr={[1, 2]}
-    camera={{ fov }}
+    camera={{ fov, position: cameraPosition }}
     resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}
     style={{ background }}
   >
