9d1fd7fb176b1f2117b6053c077412c4cd0fc013
Elar Huik <ehexec@gmail.com>
2022-04-22 17:05:29 +0300

    feat: add animation helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,3 +2,5 @@ export const isValidGlbUrl = (url: string) => {
   const expression = new RegExp(/(.glb|.glb[?].*)$/g);
   return expression.test(url);
 };
+export const clamp = (value: number, max: number, min: number): number => Math.min(Math.max(min, value), max);
+export const lerp = (start: number, end: number, time = 0.1): number => start * (1 - time) + end * time;
