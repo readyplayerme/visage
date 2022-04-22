65a836f042bee1b4025ba6a4ea39c0364f31f2e3
Elar Huik <ehexec@gmail.com>
2022-04-22 11:23:33 +0300

    feat: use helper method for glb validation

--- src/Exhibit/Exhibit.component.tsx
+++ src/Exhibit/Exhibit.component.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, FC } from 'react';
 import { PresentationControls, Environment, ContactShadows } from '@react-three/drei';
 import type { PresetsType } from '@react-three/drei/helpers/environment-assets';
+import { isValidGlbUrl } from 'src/helpers';
 import { BaseCanvas } from '../BaseCanvas';
 import { FloatingModel } from '../FloatingModel';
 
@@ -31,35 +32,31 @@ export const Exhibit: FC<ExhibitProps> = ({
   scale = 1.0,
   backgroundColor = '#f0f0f0',
   environment = 'city'
-}) => {
-  const isValidGlbUrl = glbUrl.endsWith('.glb');
-
-  return (
-    <BaseCanvas background={backgroundColor}>
-      <Suspense fallback={null}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
-        <PresentationControls
-          global
-          config={{ mass: 2, tension: 500 }}
-          snap
-          rotation={[0, -0.3, 0]}
-          polar={[-Math.PI / 3, Math.PI / 3]}
-          azimuth={[-Math.PI / 1.4, Math.PI / 2]}
-        >
-          {isValidGlbUrl && <FloatingModel glbUrl={glbUrl} scale={scale} />}
-        </PresentationControls>
-        <ContactShadows
-          rotation-x={Math.PI / 2}
-          position={[0, -1.0, 0]}
-          opacity={0.75}
-          width={10}
-          height={10}
-          blur={2.6}
-          far={2}
-        />
-        <Environment preset={environment} />
-      </Suspense>
-    </BaseCanvas>
-  );
-};
+}) => (
+  <BaseCanvas background={backgroundColor}>
+    <Suspense fallback={null}>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
+      <PresentationControls
+        global
+        config={{ mass: 2, tension: 500 }}
+        snap
+        rotation={[0, -0.3, 0]}
+        polar={[-Math.PI / 3, Math.PI / 3]}
+        azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+      >
+        {isValidGlbUrl(glbUrl) && <FloatingModel glbUrl={glbUrl} scale={scale} />}
+      </PresentationControls>
+      <ContactShadows
+        rotation-x={Math.PI / 2}
+        position={[0, -1.0, 0]}
+        opacity={0.75}
+        width={10}
+        height={10}
+        blur={2.6}
+        far={2}
+      />
+      <Environment preset={environment} />
+    </Suspense>
+  </BaseCanvas>
+);
