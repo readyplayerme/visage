e7cabeb2ab8d154a8625e8aad1860e8501684cd7
Elar Huik <ehexec@gmail.com>
2022-04-25 18:45:13 +0300

    feat: add material normaliser

--- src/AnimationModel/AnimationModel.component.tsx
+++ src/AnimationModel/AnimationModel.component.tsx
@@ -2,7 +2,8 @@ import { useGLTF } from '@react-three/drei';
 import { SkeletonUtils } from 'three-stdlib';
 import React, { useMemo, useRef, FC } from 'react';
 import { useFrame, useGraph } from '@react-three/fiber';
-import { AnimationMixer, LinearFilter, Material, MeshStandardMaterial, Object3D } from 'three';
+import { AnimationMixer, Group } from 'three';
+import { normaliseMaterialsConfig } from 'src/helpers';
 
 interface AnimationModelProps {
   modelUrl: string;
@@ -20,36 +21,31 @@ export const AnimationModel: FC<AnimationModelProps> = ({
   rotation = defaultRotation,
   scale = 1
 }) => {
+  const ref = useRef<Group>();
   const { scene } = useGLTF(modelUrl, false);
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const { nodes, materials } = useGraph(clone);
-  const meshRef = useRef<Object3D>();
+
+  normaliseMaterialsConfig(materials);
+
   const animationSource = useGLTF(animationUrl, false);
   const mixer = new AnimationMixer(nodes.Armature);
-
   mixer.clipAction(animationSource.animations[0]).play();
   mixer.update(0);
 
-  Object.values(materials).forEach((material: Material) => {
-    const mat = material as MeshStandardMaterial;
-    if (mat.map) {
-      mat.map.minFilter = LinearFilter;
-      mat.depthWrite = true;
+  useFrame((state, delta) => {
+    mixer?.update(delta);
+    if (ref?.current) {
+      currentRotation += delta * 0.2;
+      ref.current.rotation.y = rotation + Math.sin(currentRotation) / 3;
     }
   });
 
-  useFrame((state, delta) => {
-    const ref = meshRef.current as Object3D;
-    mixer?.update(delta);
-    currentRotation += delta * 0.2;
-    ref.rotation.y = rotation + Math.sin(currentRotation) / 3;
-  });
-
   return (
-    <group ref={meshRef} rotation={[0, 0, 0]}>
+    <group ref={ref} rotation={[0, 0, 0]}>
       <primitive key="armature" object={nodes.Armature || nodes.Hips} scale={scale} />
       {Object.keys(nodes).map((key) => {
-        const node: any = nodes[key];
+        const node = nodes[key];
 
         if (node.type === 'SkinnedMesh') {
           return <primitive key={node.name} object={node} receiveShadow castShadow />;
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import { LinearFilter, MeshStandardMaterial, Material } from 'three';
+
 export const isValidGlbUrl = (url: string | string[]): boolean => {
   if (Array.isArray(url)) {
     return url.filter((url) => !isValidGlbUrl(url)).length === 0;
@@ -5,5 +7,20 @@ export const isValidGlbUrl = (url: string | string[]): boolean => {
   const expression = new RegExp(/(.glb|.glb[?].*)$/g);
   return expression.test(url);
 };
+
 export const clamp = (value: number, max: number, min: number): number => Math.min(Math.max(min, value), max);
+
 export const lerp = (start: number, end: number, time = 0.1): number => start * (1 - time) + end * time;
+
+/**
+ * Avoid texture pixelation and add depth effect.
+ */
+export const normaliseMaterialsConfig = (materials: Record<string, Material>) => {
+  Object.values(materials).forEach((material) => {
+    const mat = material as MeshStandardMaterial;
+    if (mat.map) {
+      mat.map.minFilter = LinearFilter;
+      mat.depthWrite = true;
+    }
+  });
+};
