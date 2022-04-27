dbde44e3aa4d4c4ca4917c317821c13fcf02a931
Elar Huik <ehexec@gmail.com>
2022-04-27 23:35:04 +0300

    feat: update library exports

--- src/library.ts
+++ src/library.ts
@@ -1,4 +1,8 @@
 import { Exhibit } from './Exhibit';
 import { FloatingModel } from './FloatingModel';
+import { AnimationModel } from './AnimationModel';
+import { HalfBodyModel } from './HalfBodyModel';
+import { StaticModel } from './StaticModel';
+import { Avatar, CAMERA } from './Avatar';
 
-export { Exhibit, FloatingModel };
+export { Exhibit, FloatingModel, AnimationModel, HalfBodyModel, StaticModel, Avatar, CAMERA };
