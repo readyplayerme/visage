e0b9655e3d9743de921c17c2236529b8d63def6f
Elar Huik <ehexec@gmail.com>
2022-04-27 19:05:49 +0300

    feat: update validator method

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,12 +3,17 @@ import { LinearFilter, MeshStandardMaterial, Material } from 'three';
 export const getStoryAssetPath = (publicAsset: string) =>
   `${process.env.NODE_ENV === 'production' ? '/visage' : ''}/${publicAsset}`;
 
-export const isValidGlbUrl = (url: string | string[]): boolean => {
+export const isValidGlbUrl = (url: string | string[] | undefined): boolean => {
   if (Array.isArray(url)) {
     return url.filter((url) => !isValidGlbUrl(url)).length === 0;
   }
-  const expression = new RegExp(/(.glb|.glb[?].*)$/g);
-  return expression.test(url);
+
+  if (typeof url === 'string') {
+    const expression = new RegExp(/(.glb|.glb[?].*)$/g);
+    return expression.test(url);
+  }
+
+  return false;
 };
 
 export const clamp = (value: number, max: number, min: number): number => Math.min(Math.max(min, value), max);
