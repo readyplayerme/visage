5470c032d92d635de350e06a6011b45a5289fdf9
Elar Huik <ehexec@gmail.com>
2022-04-28 11:41:38 +0300

    test: add back notice

--- src/App/App.component.tsx
+++ src/App/App.component.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>localhost playground</div>
       <div style={{ display: 'flex', height: '100%' }}>
         <div style={{ width: '50%' }}>
           <Avatar modelUrl="/male.glb" animationUrl="/maleIdle.glb" backgroundColor="#fafafa" shadows />
