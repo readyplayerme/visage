de1adc56dc71c0a99ef28d7f7f88ae5fb950e9b7
Elar Huik <ehexec@gmail.com>
2022-07-12 15:08:34 +0300

    feat(pose): export new component to package

--- src/library.ts
+++ src/library.ts
@@ -3,6 +3,7 @@ import { FloatingModel } from './components/Models/FloatingModel';
 import { AnimationModel } from './components/Models/AnimationModel';
 import { HalfBodyModel } from './components/Models/HalfBodyModel';
 import { StaticModel } from './components/Models/StaticModel';
+import { PoseModel } from './components/Models/PoseModel';
 import { Avatar, CAMERA } from './components/Avatar';
 
-export { Exhibit, FloatingModel, AnimationModel, HalfBodyModel, StaticModel, Avatar, CAMERA };
+export { Exhibit, FloatingModel, AnimationModel, HalfBodyModel, StaticModel, PoseModel, Avatar, CAMERA };
