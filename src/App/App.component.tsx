fed1bb2fda11a4350367cbdeb7f1d8eff7711aeb
BDenysovets <bdenysovets@gmail.com>
2022-07-17 22:31:19 +0300

    feat(avatar): avatar sad emotion added

--- src/App/App.component.tsx
+++ src/App/App.component.tsx
@@ -11,7 +11,7 @@ function App() {
           <Avatar
             modelUrl="https://d1a370nemizbjq.cloudfront.net/21285408-3fa1-4a18-8208-02c380f7c3f3.glb"
             // modelUrl="https://d1a370nemizbjq.cloudfront.net/f8d061fc-8f64-49d5-90b3-94fdadfc2974.glb"
-            // modelUrl="https://d1a370nemizbjq.cloudfront.net/77d2b696-ba58-4874-9837-830069467841.glb"
+            // modelUrl="https://d1a370nemizbjq.cloudfront.net/23d30aa3-7fa1-4e41-96d6-e2392c3bd0a8.glb"
             poseUrl="/male-pose-standing.glb"
             backgroundColor="#fafafa"
             shadows
