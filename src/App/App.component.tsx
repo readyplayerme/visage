c9a9344ba37688134887d24340356fa2008b5716
BDenysovets <bdenysovets@gmail.com>
2022-08-17 12:37:00 +0300

    fix(app): module scss added

--- src/App/App.component.tsx
+++ src/App/App.component.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Avatar } from 'src/components/Avatar';
-import './App.scss';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <div className="settings">
-        <div className="wrapper">
-          <h3 className="title">localhost playground</h3>
-          <div className="content">
+    <div className={styles.app}>
+      <div className={styles.settings}>
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>localhost playground</h3>
+          <div className={styles.content}>
             Paste your content in there to test the avatar props without shrinking the canvas
           </div>
         </div>
       </div>
-      <div className="container">
-        <div className="card" style={{ width: '100%' }}>
+      <div className={styles.container}>
+        <div className={styles.card}>
           <Avatar modelUrl="/male.glb" poseUrl="/male-pose-standing.glb" backgroundColor="#fafafa" shadows={false} />
         </div>
       </div>
