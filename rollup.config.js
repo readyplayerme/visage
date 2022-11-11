699c690058507d84711d6326d4b38fa3b39983fd
Elar Huik <ehexec@gmail.com>
2022-11-11 16:12:27 +0200

    fix(bundle): remove sourcemaps from package

--- rollup.config.js
+++ rollup.config.js
@@ -13,12 +13,12 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true
+        sourcemap: false
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true
+        sourcemap: false
       }
     ],
     plugins: [
