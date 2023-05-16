1b40d80bcdbc484c157d33dbe61abd0b432f49b7
Elar Huik <ehexec@gmail.com>
2023-05-16 15:36:35 +0300

    feat: add bounds container to handle fit to content on first render

---
+++ src/components/Models/BoundsModel/BoundsModel.container.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactNode, useEffect, useState, cloneElement, useMemo, ReactElement } from 'react';
+import { useBounds } from '@react-three/drei';
+import { BaseModelProps } from 'src/types';
+
+export interface BoundsModelContainerProps extends BaseModelProps {
+  modelSrc: string | Blob;
+  children?: ReactNode | ReactNode[];
+  fit?: boolean;
+}
+
+export const BoundsModelContainer: FC<BoundsModelContainerProps> = ({ modelSrc, children, fit }) => {
+  const bounds = useBounds();
+  const [fallback, setFallback] = useState<JSX.Element>(<></>);
+
+  const childModel = useMemo(
+    () =>
+      React.Children.map(children, (child) => cloneElement(child as ReactElement, { setModelFallback: setFallback })),
+    [modelSrc, children]
+  );
+
+  useEffect(() => {
+    if (fit) {
+      bounds.refresh().clip().fit().fit();
+    }
+  }, [modelSrc, fit, fallback]);
+
+  return <>{childModel}</>;
+};
