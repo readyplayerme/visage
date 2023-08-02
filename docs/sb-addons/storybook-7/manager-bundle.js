1617f85302c15e732a048ac150fd91ef9b045b4c
Zaehiel <ehexec@gmail.com>
2023-08-02 08:29:30 +0000

    Deploying to github-pages from @ readyplayerme/visage@e41707fea29966efc65fabb2f886b0aeaf382c44 🚀

---
+++ docs/sb-addons/storybook-7/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var h=__STORYBOOKADDONS__,{addons:o,types:_,mockChannel:c}=__STORYBOOKADDONS__;var u=__STORYBOOKTHEMING__,{CacheProvider:y,ClassNames:S,Global:b,ThemeProvider:x,background:C,color:N,convert:P,create:a,createCache:R,createGlobal:k,createReset:B,css:D,darken:G,ensure:K,ignoreSsrWarning:Y,isPropValid:v,jsx:I,keyframes:w,lighten:A,styled:E,themes:H,typography:M,useTheme:j,withTheme:U}=__STORYBOOKTHEMING__;var s=a({base:"light",brandImage:"/logo.png",brandUrl:"https://readyplayer.me"});o.setConfig({theme:s,panelPosition:"right",showPanel:!0});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
