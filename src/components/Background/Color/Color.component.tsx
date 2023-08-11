bbe38647866c1fc611609c6dafc2f0e5fc929d99
Elar Huik <ehexec@gmail.com>
2023-08-11 19:50:46 +0300

    chore: update prop docs

--- src/components/Background/Color/Color.component.tsx
+++ src/components/Background/Color/Color.component.tsx
@@ -2,7 +2,9 @@ import React, { FC } from 'react';
 
 export type BackgroundColorProps = {
   /**
-   * CSS-style string. For example: '#000' or 'red'.
+   * CSS-style string. Accepts Hexadeximal, RGB, X11 color name, HSL string
+   * For example: '#000', 'red', 'rgb(255, 0, 0)'.
+   * Doesn't support CSS gradients.
    */
   color: string;
 };
