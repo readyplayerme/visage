3ab3d8b7770b249e13f4243066cb97f92477025f
Elar Huik <ehexec@gmail.com>
2023-08-16 13:23:26 +0300

    chore: add new example

--- src/components/Avatar/Examples.stories.tsx
+++ src/components/Avatar/Examples.stories.tsx
@@ -49,8 +49,6 @@ const ignoreArgTypesOnExamples = keysToIgnore.reduce(
   {} as Record<string, typeof disableTable>
 );
 
-const Template: StoryFn<typeof Avatar> = (args) => <Avatar style={{ background: 'rgb(9,20,26)' }} {...args} />;
-
 export const FloatingSparkles: StoryFn<typeof SparklesDrei> = (args) => (
   <Avatar
     modelSrc={getStoryAssetPath('female.glb')}
@@ -79,7 +77,11 @@ FloatingSparkles.argTypes = {
   ...ignoreArgTypesOnExamples
 };
 
-export const SpawnEffectAndAnimation: StoryFn<typeof Avatar> = Template.bind({});
+export const SpawnEffectAndAnimation: StoryFn<typeof Avatar> = (args) => (
+  <Avatar {...args}>
+    <Sparkles color="white" count={50} opacity={0.9} scale={5} size={0.5} speed={0.35} />
+  </Avatar>
+);
 SpawnEffectAndAnimation.args = {
   onLoadedEffect: {
     src: getStoryAssetPath('spawn-effect.glb'),
@@ -100,4 +102,70 @@ SpawnEffectAndAnimation.argTypes = {
   onLoadedAnimation: { control: { disable: false } },
   modelSrc: { control: { disable: false } },
   animationSrc: { control: { disable: false } }
+};
+
+/**
+ * https://models.readyplayer.me/64d61e9e17883fd73ebe5eb7.glb
+ * https://models.readyplayer.me/64d61f67f0367d07504924be.glb
+ * https://models.readyplayer.me/64d62255f0367d0750492913.glb
+ * https://models.readyplayer.me/64d622ce17883fd73ebe64cd.glb
+ * https://models.readyplayer.me/64d6235e2d3bea6e4267b01d.glb
+ * https://models.readyplayer.me/64d5d4eb651a0d350005c672.glb
+ */
+
+export const modelPresets = {
+  one: 'https://models.readyplayer.me/64d61e9e17883fd73ebe5eb7.glb?morphTargets=Default,ARKit&lod=0',
+  two: 'https://models.readyplayer.me/64d61f67f0367d07504924be.glb',
+  three: 'https://models.readyplayer.me/64d62255f0367d0750492913.glb',
+  four: 'https://models.readyplayer.me/64d622ce17883fd73ebe64cd.glb',
+  five: 'https://models.readyplayer.me/64d6235e2d3bea6e4267b01d.glb',
+  six: 'https://models.readyplayer.me/64d5d4eb651a0d350005c672.glb'
+};
+export const Showcase: StoryFn<typeof Avatar> = (args) => (
+  <Avatar {...args}>
+    <Sparkles color="white" count={50} opacity={0.9} scale={5} size={0.5} speed={0.35} />
+  </Avatar>
+);
+Showcase.args = {
+  onLoadedAnimation: {
+    src: getStoryAssetPath('M_Standing_Idle_Variations_002.fbx'),
+    loop: 1
+  },
+  cameraTarget: CAMERA.TARGET.FULL_BODY.FEMALE,
+  cameraInitialDistance: CAMERA.CONTROLS.FULL_BODY.MAX_DISTANCE,
+  modelSrc: 'https://models.readyplayer.me/64d61e9e17883fd73ebe5eb7.glb?morphTargets=Default,ARKit&lod=0',
+  animationSrc: getStoryAssetPath('M_Standing_Idle_001.fbx'),
+  bloom: {
+    luminanceThreshold: 1.0,
+    luminanceSmoothing: 1.0,
+    mipmapBlur: true,
+    kernelSize: 1,
+    intensity: 1,
+    materialIntensity: 6
+  },
+  style: { background: '#282038' },
+  dpr: 2,
+  ambientLightColor: '#ffffff',
+  dirLightColor: '#ffffff',
+  spotLightColor: '#adbfe5',
+  ambientLightIntensity: 0,
+  dirLightIntensity: 2.2,
+  spotLightIntensity: 0.5,
+  environment: 'apartment',
+  shadows: true,
+  emotion: {
+    jawOpen: 0.1,
+    mouthSmileLeft: 0.2,
+    mouthSmileRight: 0.1,
+    mouthPressLeft: 0.1,
+    cheekSquintLeft: 0.3,
+    eyeLookOutLeft: 0.6,
+    eyeLookInRight: 0.6,
+    mouthDimpleLeft: 0.3
+  }
+};
+Showcase.argTypes = {
+  onLoadedAnimation: { control: { disable: false } },
+  animationSrc: { control: { disable: false } },
+  modelSrc: { options: Object.values(modelPresets), control: { type: 'select' } }
 };
