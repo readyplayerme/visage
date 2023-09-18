fc038798125ffd68d30c10d24f35defa77c2d3b6
Elar Huik <ehexec@gmail.com>
2023-09-18 16:53:48 +0300

    feat: add reflective floor example

--- src/components/FloorReflection/FloorReflection.component.tsx
+++ src/components/FloorReflection/FloorReflection.component.tsx
@@ -18,11 +18,43 @@ export interface FloorReflectionProps {
   roughness?: number;
   color?: string;
 }
-export const FloorReflection: FC<FloorReflectionProps> = (props) => (
+export const FloorReflection: FC<FloorReflectionProps> = ({
+  resolution = 2048,
+  mixBlur = 1.5,
+  mixStrength = 80,
+  metalness = 0.5,
+  blur = [300, 200],
+  mirror = 1,
+  minDepthThreshold = 0.4,
+  maxDepthThreshold = 1.4,
+  depthScale = 1.2,
+  depthToBlurRatioBias = 1,
+  distortion = 0,
+  mixContrast = 1,
+  reflectorOffset = 0,
+  roughness = 1,
+  ...props
+}) => (
   <group position={[0, -0.01, 0]}>
     <mesh rotation={[-Math.PI / 2, 0, 0]}>
       <planeGeometry args={[20, 5]} />
-      <MeshReflectorMaterial {...props} />
+      <MeshReflectorMaterial
+        resolution={resolution}
+        mixBlur={mixBlur}
+        mixStrength={mixStrength}
+        metalness={metalness}
+        blur={blur}
+        mirror={mirror}
+        minDepthThreshold={minDepthThreshold}
+        maxDepthThreshold={maxDepthThreshold}
+        depthScale={depthScale}
+        depthToBlurRatioBias={depthToBlurRatioBias}
+        distortion={distortion}
+        mixContrast={mixContrast}
+        reflectorOffset={reflectorOffset}
+        roughness={roughness}
+        {...props}
+      />
     </mesh>
   </group>
 );
