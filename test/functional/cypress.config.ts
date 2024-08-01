9aedd87213f03af2407053a917767b0998eb2ed5
Eric Jansen <ericjansen@readyplayer.me>
2024-08-01 14:01:55 +0200

    feat: wip visual test

--- test/functional/cypress.config.ts
+++ test/functional/cypress.config.ts
@@ -4,7 +4,11 @@ import getCompareSnapshotsPlugin from 'cypress-image-diff-js/plugin';
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      return getCompareSnapshotsPlugin(on, config);
+      return getCompareSnapshotsPlugin(on, {
+        ...config,
+        generateHtmlReport: false,
+        generateJsonReport: false
+      });
     },
     viewportWidth: 576,
     viewportHeight: 1024,
