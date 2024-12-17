41c09b8431072931280fcf92e4e700de0cc78e5c
dan-rpm <dan@wolf3d.io>
2024-12-17 12:51:53 +0200

    feat: disable zoom via props

--- src/components/Avatar/Avatar.component.tsx
+++ src/components/Avatar/Avatar.component.tsx
@@ -313,6 +313,20 @@ const Avatar: FC<AvatarProps> = ({
   useEffect(() => triggerCallback(onLoading), [modelSrc, animationSrc, onLoading]);
 
   const enablePostProcessing = Boolean(effects?.ambientOcclusion || effects?.bloom || effects?.vignette);
+  const cameraControlsMinDistance = useMemo(() => {
+    if (controlsMinDistance) {
+      return controlsMinDistance;
+    }
+
+    return halfBody ? CAMERA.CONTROLS.HALF_BODY.MIN_DISTANCE : CAMERA.CONTROLS.FULL_BODY.MIN_DISTANCE;
+  }, [controlsMinDistance, halfBody]);
+  const cameraControlsMsxDistance = useMemo(() => {
+    if (controlsMaxDistance) {
+      return controlsMaxDistance;
+    }
+
+    return halfBody ? CAMERA.CONTROLS.HALF_BODY.MAX_DISTANCE : CAMERA.CONTROLS.FULL_BODY.MAX_DISTANCE;
+  }, [controlsMaxDistance, halfBody]);
 
   return (
     <BaseCanvas
@@ -328,8 +342,8 @@ const Avatar: FC<AvatarProps> = ({
         cameraTarget={cameraTarget}
         cameraInitialDistance={cameraInitialDistance}
         cameraZoomTarget={cameraZoomTarget}
-        controlsMinDistance={controlsMinDistance}
-        controlsMaxDistance={controlsMaxDistance}
+        controlsMinDistance={cameraControlsMinDistance}
+        controlsMaxDistance={cameraControlsMsxDistance}
         updateCameraTargetOnZoom={!halfBody}
       />
       {AvatarModel}
