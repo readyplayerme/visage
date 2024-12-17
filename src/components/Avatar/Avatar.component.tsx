0bd3b55c71e6c30c5a3f2b8655b38aaf20d39f19
dan-rpm <117362126+dan-rpm@users.noreply.github.com>
2024-12-17 13:30:21 +0200

    feat(zoom): disable zoom via props (#108)
    * feat: disable zoom via props
    
    * feat: disable zoom via props
    
    * feat: disable zoom via props
    
    * feat: disable zoom via props
    
    * fix: typo
    
    ---------
    
    Co-authored-by: dan-rpm <dan@wolf3d.io>

--- src/components/Avatar/Avatar.component.tsx
+++ src/components/Avatar/Avatar.component.tsx
@@ -160,6 +160,8 @@ export interface AvatarProps extends LightingProps, EnvironmentProps, Omit<BaseM
    */
   materialConfig?: MaterialConfiguration;
   onAnimationEnd?: (action: AnimationAction) => void;
+  controlsMinDistance?: number;
+  controlsMaxDistance?: number;
 }
 
 /**
@@ -205,7 +207,9 @@ const Avatar: FC<AvatarProps> = ({
   lightTarget,
   fov = 50,
   onAnimationEnd,
-  materialConfig
+  materialConfig,
+  controlsMinDistance,
+  controlsMaxDistance
 }) => {
   const setSpawnState = useSetAtom(spawnState);
 
@@ -309,6 +313,20 @@ const Avatar: FC<AvatarProps> = ({
   useEffect(() => triggerCallback(onLoading), [modelSrc, animationSrc, onLoading]);
 
   const enablePostProcessing = Boolean(effects?.ambientOcclusion || effects?.bloom || effects?.vignette);
+  const cameraControlsMinDistance = useMemo(() => {
+    if (controlsMinDistance) {
+      return controlsMinDistance;
+    }
+
+    return halfBody ? CAMERA.CONTROLS.HALF_BODY.MIN_DISTANCE : CAMERA.CONTROLS.FULL_BODY.MIN_DISTANCE;
+  }, [controlsMinDistance, halfBody]);
+  const cameraControlsMaxDistance = useMemo(() => {
+    if (controlsMaxDistance) {
+      return controlsMaxDistance;
+    }
+
+    return halfBody ? CAMERA.CONTROLS.HALF_BODY.MAX_DISTANCE : CAMERA.CONTROLS.FULL_BODY.MAX_DISTANCE;
+  }, [controlsMaxDistance, halfBody]);
 
   return (
     <BaseCanvas
@@ -324,8 +342,8 @@ const Avatar: FC<AvatarProps> = ({
         cameraTarget={cameraTarget}
         cameraInitialDistance={cameraInitialDistance}
         cameraZoomTarget={cameraZoomTarget}
-        controlsMinDistance={halfBody ? CAMERA.CONTROLS.HALF_BODY.MIN_DISTANCE : CAMERA.CONTROLS.FULL_BODY.MIN_DISTANCE}
-        controlsMaxDistance={halfBody ? CAMERA.CONTROLS.HALF_BODY.MAX_DISTANCE : CAMERA.CONTROLS.FULL_BODY.MAX_DISTANCE}
+        controlsMinDistance={cameraControlsMinDistance}
+        controlsMaxDistance={cameraControlsMaxDistance}
         updateCameraTargetOnZoom={!halfBody}
       />
       {AvatarModel}
