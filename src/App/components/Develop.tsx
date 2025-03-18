3ed682b0d6aa2ba453eb5d24fa3b6367c5e503dd
dan-rpm <dan@wolf3d.io>
2025-03-18 18:02:10 +0200

    fix: use fbx model absolute path instead of relative

--- src/App/components/Develop.tsx
+++ src/App/components/Develop.tsx
@@ -3,6 +3,7 @@ import { StatsGl } from '@react-three/drei';
 
 import { Avatar } from 'src/components/Avatar';
 import { EnvironmentModel } from 'src/components/Models';
+import { getAnimation } from 'src/services/Animation.service';
 
 import { SettingsPanel } from './SettingsPanel';
 
@@ -13,7 +14,7 @@ export const AvatarDevelop: React.FC = () => (
     <Avatar
       modelSrc="https://models.readyplayer.me/64d61e9e17883fd73ebe5eb7.glb?morphTargets=ARKit,Eyes Extra&textureAtlas=none&lod=0"
       shadows
-      animationSrc="/male-idle-2.fbx"
+      animationSrc={getAnimation('male-idle-2.fbx')}
       style={{ background: 'rgb(9,20,26)' }}
       fov={45}
       effects={{
